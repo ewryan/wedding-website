99173b903f9c0eddabeeeb626da18a59dd16a237
Eric Ryan <eryan@gnip.com>
2011-11-25 16:35:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingWebsite::Application.config.secret_token = '3e2f5750b441b81739389b49713b37a9f6a7c3284e03d8d75113c36436f2f5f99d3d1ef8980f4bd7640b96b52b7044ef928638d0ce3c37ab73fbe55a7ea92bbb'
