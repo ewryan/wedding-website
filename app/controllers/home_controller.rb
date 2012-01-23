b8fdb3d82ee0818615e2f8228f703ba00be8da5f
Eric Ryan <eryan@gnip.com>
2012-01-22 21:05:29 -0700

    er: add registry info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
 
+  def registry
+    @target_url = 'http://www.target.com/wedd/registry/dlMymye7T4ljgcV3wMRKDw'
+    @macys_url = 'http://www1.macys.com/registry/wedding/guest/?registryId=668558'
+    @pottery_barn_url = 'http://www.potterybarn.com/registry/2933976/registry-list.html'
+  end
+
   def inbound_email
     errors = validate_inbound_email params
     respond_to do |format|
@@ -16,10 +22,10 @@ class HomeController < ApplicationController
 
   def validate_inbound_email params
     errors = []
-    errors <<  "Email can't be blank.  " if params[:email].blank?
-    errors <<  "Name can't be blank.  " if params[:name].blank?
-    errors <<  "Subject can't be blank.  " if params[:subject].blank?
-    errors <<  "Message can't be blank." if params[:message].blank?
+    errors << "Email can't be blank.  " if params[:email].blank?
+    errors << "Name can't be blank.  " if params[:name].blank?
+    errors << "Subject can't be blank.  " if params[:subject].blank?
+    errors << "Message can't be blank." if params[:message].blank?
     errors
   end
 
