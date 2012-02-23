88941c9245179584b2e7790f42b5f467a5bdcd5b
Eric Ryan <eryan@gnip.com>
2012-02-22 23:03:41 -0700

    er: persist phone and email

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -2,9 +2,9 @@ class Rsvp < ActiveRecord::Base
 
   def to_s
     if primary
-      "#{title}, #{first_name}, #{last_name}, #{phone}, #{email}"
+      "#{title} #{first_name} #{last_name} #{phone} #{email}"
     else
-      "#{title}, #{first_name}, #{last_name}"
+      "#{title} #{first_name} #{last_name}"
     end
   end
 
