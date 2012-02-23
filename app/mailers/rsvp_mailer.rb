042765aa295093a4bd218af2ba332a68021f4eed
Eric Ryan <eryan@gnip.com>
2012-02-22 22:49:56 -0700

    er: fix mail body

--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -4,7 +4,7 @@ class RsvpMailer < ActionMailer::Base
     @primary = primary
     @guests  = guests
     @message = message
-    mail(:from => "no-reply@ericandamanda.org", :to => 'ewryan@gmail.com', :cc => 'aegeorge4@gmail.com', :subject => "New RSVP: #{primary.first_name} #{primary.last_name} + #{@guests.size}")
+    mail(:from => '"amandaanderic.org" <noreply@ericandamanda.org>', :to => 'ewryan@gmail.com', :cc => 'aegeorge4@gmail.com', :subject => "New RSVP: #{primary.first_name} #{primary.last_name} + #{@guests.size}")
   end
 
 end
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,5 +1,7 @@
 class Rsvp < ActiveRecord::Base
 
-
+  def to_s
+    "#{title}, #{first_name}, #{last_name}"
+  end
 
 end
