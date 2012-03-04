38ede5c695503833d99c1f1e1903c68d3d82f66d
Eric Ryan <eryan@gnip.com>
2012-03-04 09:52:16 -0700

    er: fix email to show attending/not

--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -4,7 +4,10 @@ class RsvpMailer < ActionMailer::Base
     @primary = primary
     @guests  = guests
     @message = message
-    mail(:from => '"amandaanderic.org" <noreply@ericandamanda.org>', :to => 'ewryan@gmail.com', :cc => 'aegeorge4@gmail.com', :subject => "New RSVP: #{primary.first_name} #{primary.last_name} + #{@guests.size}")
+
+    subject_prefix = primary.attending? ? "Attending" : "Not Attending"
+
+    mail(:from => '"amandaanderic.org" <noreply@ericandamanda.org>', :to => 'ewryan@gmail.com', :cc => 'aegeorge4@gmail.com', :subject => "New RSVP - #{subject_prefix} : #{primary.first_name} #{primary.last_name} + #{@guests.size}")
   end
 
 end
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -2,14 +2,13 @@ class Rsvp < ActiveRecord::Base
 
   def to_s
     if primary
-      "#{title} #{first_name} #{last_name}, email='#{phone}', phone='#{email}'"
+      "#{title} #{first_name} #{last_name}, email='#{phone}', phone='#{email}', attending=#{attending}"
     else
       "#{title} #{first_name} #{last_name}"
     end
   end
 
   def attending?
-
     "yes" == attending
   end
 
